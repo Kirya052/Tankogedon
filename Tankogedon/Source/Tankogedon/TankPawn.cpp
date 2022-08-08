316dc26979f8f30e988032bbb2746a7696bfe456
Kirya052 <kiryakonev01@mail.ru>
2022-08-08 18:47:06 +0300

    Add TankMovement

---
+++ Tankogedon/Source/Tankogedon/TankPawn.cpp
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankPawn.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/BoxComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+ATankPawn::ATankPawn()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	BoxCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("RootComponent"));
+	RootComponent = BoxCollision;
+
+	BodyMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BodyMesh"));
+	BodyMesh->SetupAttachment(BoxCollision);
+
+	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TurretMesh"));
+	TurretMesh->SetupAttachment(BodyMesh);
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(BoxCollision);
+	SpringArm->bDoCollisionTest = false;
+	SpringArm->bInheritPitch = false;
+	SpringArm->bInheritRoll = false;
+	SpringArm->bInheritYaw = false;
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
+}
+
+void ATankPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	//GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Red, FString::Printf(TEXT("Hello")));
+
+	Movement(DeltaTime);
+}
+
+void ATankPawn::MoveForward(float Value)
+{
+	targetForwardAxisValue = Value;
+}
+
+void ATankPawn::MoveRight(float Value)
+{
+	targetRightAxisValue = Value;
+}
+
+void ATankPawn::Movement(float DeltaTime)
+{
+	FVector CurrentPosition = GetActorLocation();
+	FVector ForwardVector = GetActorForwardVector();
+	FVector RightVector = GetActorRightVector();
+
+	FVector ForwardMovement = ForwardVector * MoveSpeed * targetForwardAxisValue * DeltaTime;
+	FVector RightMovement = RightVector * MoveSpeed * targetRightAxisValue * DeltaTime;
+	FVector movePosition = CurrentPosition + ForwardMovement + RightMovement;
+	SetActorLocation(movePosition, true);
+}
---
+++ Tankogedon/Source/Tankogedon/TankPawn.h
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "TankPawn.generated.h"
+
+class UStaticMeshComponent;
+UCLASS()
+class TANKOGEDON_API ATankPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	ATankPawn();
+
+	virtual void Tick(float DeltaTime) override;
+
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+	UStaticMeshComponent* BodyMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+	UStaticMeshComponent* TurretMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+	class UBoxComponent* BoxCollision;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+	class USpringArmComponent* SpringArm;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+	class UCameraComponent* Camera;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float MoveSpeed = 100.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float RotationSpeed = 100.0f;
+
+private:
+	float targetForwardAxisValue = 0.0f;
+	float targetRightAxisValue = 0.0f;
+
+	void Movement(float DeltaTime);
+};
